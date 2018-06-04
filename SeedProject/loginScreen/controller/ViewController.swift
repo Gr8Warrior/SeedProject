d6d47484ddfd5bda7fcf076de7bfdf8063010d95
shailendra.suriyal <shailendra.suriyal@intimetec.com>
2018-06-04 10:48:04 -0700

    Added swiftlint.yml for linting

--- SeedProject/loginScreen/controller/ViewController.swift
+++ SeedProject/loginScreen/controller/ViewController.swift
@@ -7,19 +7,15 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        edgesForExtendedLayout = UIRectEdge()
+        view.backgroundColor = UIColor.cyan
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
