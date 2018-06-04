46060d069a7d693664b53ee83c20554b7c569270
shailendra.suriyal <shailendra.suriyal@intimetec.com>
2018-06-04 10:12:08 -0700

    Initial Commit

---
+++ SeedProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeedProject
+//
+//  Created by intime on 04/06/18.
+//  Copyright © 2018 In Time Tec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
