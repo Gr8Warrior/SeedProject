5cdabb08a478c16eaa37a5be46ee99021d59a7ce
shailendra.suriyal <shailendra.suriyal@intimetec.com>
2018-06-05 09:55:49 +0530

    Added vc for createAccount

---
+++ SeedProject/createAccountScreen/controller/CreateAccountViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SeedProject
+//
+//  Created by intime on 04/06/18.
+//  Copyright © 2018 In Time Tec. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        edgesForExtendedLayout = UIRectEdge()
+        view.backgroundColor = UIColor.cyan
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func createAccount(_ sender: Any) {
+        
+    }
+}
